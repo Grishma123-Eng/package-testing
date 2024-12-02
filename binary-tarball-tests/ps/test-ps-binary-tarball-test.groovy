4462fffda203e021243fce9c227f765c240a767f
Grishma123-Eng <grishmashah121@gmail.com>
2024-12-02 19:36:04 +0530

    adding parameter to tarball script

--- binary-tarball-tests/ps/test-ps-binary-tarball-test.groovy
+++ binary-tarball-tests/ps/test-ps-binary-tarball-test.groovy
@@ -31,23 +31,20 @@ pipeline {
                     unzip master.zip
                     rm -f master.zip
                     mv "package-testing-master" package-testing
-                        
+
                     def PS_VERSION = sh(
-                    script: """ grep ${PRODUCT_TO_TEST}_VER VERSIONS | awk -F= '{print $2}' | sed 's/"//g' """,
-                    returnStdout: true
+                        script: """ grep ${PRODUCT_TO_TEST}_VER VERSIONS | awk -F= '{print $2}' | sed 's/"//g' """,
+                        returnStdout: true
                     ).trim()
 
                     def PS_REVISION = sh(
-                    script: """ grep ${PRODUCT_TO_TEST}_REV VERSIONS | awk -F= '{print $2}' | sed 's/"//g' """,
-                    returnStdout: true
+                        script: """ grep ${PRODUCT_TO_TEST}_REV VERSIONS | awk -F= '{print $2}' | sed 's/"//g' """,
+                        returnStdout: true
                     ).trim()
-                        
+
                     echo "PS_VERSION: ${PS_VERSION}"
                     echo "PS_REVISION: ${PS_REVISION}"
-                    '''
-                    // Set the build display name
-                              //  currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                }
+                '''
             }
         }
         stage('Binary tarball test') {
@@ -55,11 +52,11 @@ pipeline {
                 stage('Ubuntu Noble') {
                     agent {
                         label "min-noble-x64"
+                    }
+                    steps {
+                        script {
+                            currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
                         }
-                        steps {
-                            script {
-                                currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                            }
                         sh '''
                             echo ${BUILD_TYPE_MINIMAL}
                             MINIMAL=""
@@ -229,69 +226,9 @@ pipeline {
                             wget -q ${TARBALL_LINK}${TARBALL_NAME}
                             ./run.sh || true
                         '''
-                        junit 'package-testing/binary-tarball-tests/ps/report.xml'
-                    }
-                }
-                stage('Oracle Linux 8') {
-                    agent {
-                        label "min-ol-8-x64"
-                    }
-                    steps {
-                        script {
-                            currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                        }
-                        sh '''
-                            echo ${BUILD_TYPE_MINIMAL}
-                            MINIMAL=""
-                            if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
-                                MINIMAL="-minimal"
-                            fi
-                            if [ -f /usr/bin/yum ]; then
-                                sudo yum install -y git wget
-                            else
-                                sudo apt install -y git wget
-                            fi
-                            TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.28${MINIMAL}.tar.gz"
-                            TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
-                            rm -rf package-testing
-                            git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
-                            cd package-testing/binary-tarball-tests/ps
-                            wget -q ${TARBALL_LINK}${TARBALL_NAME}
-                            ./run.sh || true
-                        '''
-                        junit 'package-testing/binary-tarball-tests/ps/report.xml'
-                    }
-                }
-                stage('Centos 7') {
-                    agent {
-                        label "min-centos-7-x64"
-                    }
-                    steps {
-                        script {
-                            currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                        }
-                        sh '''
-                            echo ${BUILD_TYPE_MINIMAL}
-                            MINIMAL=""
-                            if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
-                                MINIMAL="-minimal"
-                            fi
-                            if [ -f /usr/bin/yum ]; then
-                                sudo yum install -y git wget
-                            else
-                                sudo apt install -y git wget
-                            fi
-                            TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.17${MINIMAL}.tar.gz"
-                            TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
-                            rm -rf package-testing
-                            git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
-                            cd package-testing/binary-tarball-tests/ps
-                            wget -q ${TARBALL_LINK}${TARBALL_NAME}
-                            ./run.sh || true
-                        '''
-                        junit 'package-testing/binary-tarball-tests/ps/report.xml'
                     }
                 }
             }
         }
     }
+}
