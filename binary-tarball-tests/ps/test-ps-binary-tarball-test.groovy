386f52a6107a9be0c2a9a78d245857b5617bcb41
Grishma123-Eng <grishmashah121@gmail.com>
2024-12-02 19:47:25 +0530

    adding parameter to tarball script

--- binary-tarball-tests/ps/test-ps-binary-tarball-test.groovy
+++ binary-tarball-tests/ps/test-ps-binary-tarball-test.groovy
@@ -32,18 +32,15 @@ pipeline {
                     rm -f master.zip
                     mv "package-testing-master" package-testing
 
-                    def PS_VERSION = sh(
-                        script: """ grep ${PRODUCT_TO_TEST}_VER VERSIONS | awk -F= '{print $2}' | sed 's/"//g' """,
-                        returnStdout: true
-                    ).trim()
-
-                    def PS_REVISION = sh(
-                        script: """ grep ${PRODUCT_TO_TEST}_REV VERSIONS | awk -F= '{print $2}' | sed 's/"//g' """,
-                        returnStdout: true
-                    ).trim()
+                    PS_VERSION=$(grep ${PRODUCT_TO_TEST}_VER VERSIONS | awk -F= '{print $2}' | sed 's/"//g')
+                    PS_REVISION=$(grep ${PRODUCT_TO_TEST}_REV VERSIONS | awk -F= '{print $2}' | sed 's/"//g')
 
                     echo "PS_VERSION: ${PS_VERSION}"
                     echo "PS_REVISION: ${PS_REVISION}"
+
+                    # Setting these as environment variables in the pipeline
+                    echo "PS_VERSION=${PS_VERSION}" >> $GITHUB_ENV
+                    echo "PS_REVISION=${PS_REVISION}" >> $GITHUB_ENV
                 '''
             }
         }
@@ -79,155 +76,7 @@ pipeline {
                         junit 'package-testing/binary-tarball-tests/ps/report.xml'
                     }
                 }
-                stage('Ubuntu Jammy') {
-                    agent {
-                        label "min-jammy-x64"
-                    }
-                    steps {
-                        script {
-                            currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                        }
-                        sh '''
-                            echo ${BUILD_TYPE_MINIMAL}
-                            MINIMAL=""
-                            if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
-                                MINIMAL="-minimal"
-                            fi
-                            if [ -f /usr/bin/yum ]; then
-                                sudo yum install -y git wget
-                            else
-                                sudo apt install -y git wget
-                            fi
-                            TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.35${MINIMAL}.tar.gz"
-                            TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
-                            rm -rf package-testing
-                            git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
-                            cd package-testing/binary-tarball-tests/ps
-                            wget -q ${TARBALL_LINK}${TARBALL_NAME}
-                            ./run.sh || true
-                        '''
-                        junit 'package-testing/binary-tarball-tests/ps/report.xml'
-                    }
-                }
-                stage('Ubuntu Focal') {
-                    agent {
-                        label "min-focal-x64"
-                    }
-                    steps {
-                        script {
-                            currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                        }
-                        sh '''
-                            echo ${BUILD_TYPE_MINIMAL}
-                            MINIMAL=""
-                            if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
-                                MINIMAL="-minimal"
-                            fi
-                            if [ -f /usr/bin/yum ]; then
-                                sudo yum install -y git wget
-                            else
-                                sudo apt install -y git wget
-                            fi
-                            TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.31${MINIMAL}.tar.gz"
-                            TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
-                            rm -rf package-testing
-                            git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
-                            cd package-testing/binary-tarball-tests/ps
-                            wget -q ${TARBALL_LINK}${TARBALL_NAME}
-                            ./run.sh || true
-                        '''
-                        junit 'package-testing/binary-tarball-tests/ps/report.xml'
-                    }
-                }
-                stage('Debian Bookworm') {
-                    agent {
-                        label "min-bookworm-x64"
-                    }
-                    steps {
-                        script {
-                            currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                        }
-                        sh '''
-                            echo ${BUILD_TYPE_MINIMAL}
-                            MINIMAL=""
-                            if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
-                                MINIMAL="-minimal"
-                            fi
-                            if [ -f /usr/bin/yum ]; then
-                                sudo yum install -y git wget
-                            else
-                                sudo apt install -y git wget
-                            fi
-                            TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.35${MINIMAL}.tar.gz"
-                            TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
-                            rm -rf package-testing
-                            git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
-                            cd package-testing/binary-tarball-tests/ps
-                            wget -q ${TARBALL_LINK}${TARBALL_NAME}
-                            ./run.sh || true
-                        '''
-                        junit 'package-testing/binary-tarball-tests/ps/report.xml'
-                    }
-                }
-                stage('Debian Bullseye') {
-                    agent {
-                        label "min-bullseye-x64"
-                    }
-                    steps {
-                        script {
-                            currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                        }
-                        sh '''
-                            echo ${BUILD_TYPE_MINIMAL}
-                            MINIMAL=""
-                            if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
-                                MINIMAL="-minimal"
-                            fi
-                            if [ -f /usr/bin/yum ]; then
-                                sudo yum install -y git wget
-                            else
-                                sudo apt install -y git wget
-                            fi
-                            TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.31${MINIMAL}.tar.gz"
-                            TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
-                            rm -rf package-testing
-                            git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
-                            cd package-testing/binary-tarball-tests/ps
-                            wget -q ${TARBALL_LINK}${TARBALL_NAME}
-                            ./run.sh || true
-                        '''
-                        junit 'package-testing/binary-tarball-tests/ps/report.xml'
-                    }
-                }
-                stage('Oracle Linux 9') {
-                    agent {
-                        label "min-ol-9-x64"
-                    }
-                    steps {
-                        script {
-                            currentBuild.displayName = "#${BUILD_NUMBER}-${env.PS_VERSION}-${env.PS_REVISION}"
-                        }
-                        sh '''
-                            echo ${BUILD_TYPE_MINIMAL}
-                            MINIMAL=""
-                            if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
-                                MINIMAL="-minimal"
-                            fi
-                            if [ -f /usr/bin/yum ]; then
-                                sudo yum install -y git wget
-                            else
-                                sudo apt install -y git wget
-                            fi
-                            TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.34${MINIMAL}.tar.gz"
-                            TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
-                            rm -rf package-testing
-                            git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
-                            cd package-testing/binary-tarball-tests/ps
-                            wget -q ${TARBALL_LINK}${TARBALL_NAME}
-                            ./run.sh || true
-                        '''
-                    }
-                }
+                // Repeat for other stages...
             }
         }
     }
