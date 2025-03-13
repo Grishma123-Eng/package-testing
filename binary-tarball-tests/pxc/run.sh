c2ac9277d8e613641e7b741ec78fae4b7ca72ded
Grishma123-Eng <grishmashah121@gmail.com>
2025-03-13 14:52:12 +0530

    adding pro part in sh script

--- binary-tarball-tests/pxc/run.sh
+++ binary-tarball-tests/pxc/run.sh
@@ -5,8 +5,9 @@ PXC_MAJOR_VERSION="$(echo ${PXC_VERSION}|cut -d'.' -f1,2)"
 
 echo "Installing dependencies..."
 if [ -f /etc/redhat-release ]; then
-  sudo yum install -y libaio numactl openssl-devel socat lsof
-  sudo yum install -y perl-Data-Dumper libev
+  sudo yum install -y libaio numactl openssl socat lsof
+  # below needed for 5.6 mysql_install_db
+  sudo yum install -y perl-Data-Dumper
   if [ $(grep -c "release 6" /etc/redhat-release) -eq 1 ]; then
     sudo ../../centos6.sh
     sudo yum install -y rh-python36 rh-python36-python-pip
@@ -14,13 +15,14 @@ if [ -f /etc/redhat-release ]; then
   else
     sudo yum install -y python3 python3-pip
   fi
+  sudo yum install -y libev
   if [ "${PXC_MAJOR_VERSION}" = "5.7" ]; then
     sudo yum install -y https://repo.percona.com/yum/percona-release-latest.noarch.rpm
     sudo percona-release enable pxb-24 testing
     sudo yum install -y percona-xtrabackup-24
   fi
 else
-  UCF_FORCE_CONFOLD=1 DEBIAN_FRONTEND=noninteractive sudo -E apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq -y install openssl libssl-dev
+  UCF_FORCE_CONFOLD=1 DEBIAN_FRONTEND=noninteractive sudo -E apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq -y install openssl
   if [[ $(lsb_release -sc) == 'xenial' ]]; then
     DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:deadsnakes/ppa
     DEBIAN_FRONTEND=noninteractive sudo apt update
@@ -31,7 +33,7 @@ else
   else
     sudo apt install -y python3 python3-pip
   fi
-  sudo apt install -y python3 python3-pip libaio1 libnuma1 socat lsof curl libev4 libssl3
+  sudo apt install -y python3 python3-pip libaio1 libnuma1 socat lsof curl libev4
   if [ "${PXC_MAJOR_VERSION}" = "5.7" ]; then
     wget -q https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
     sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
@@ -40,23 +42,6 @@ else
     sudo apt-get install -y percona-xtrabackup-24
   fi
 fi
-
-echo "Fixing OpenSSL shared libraries..."
-if ! ldconfig -p | grep -q "libssl.so.3"; then
-  echo "Creating symbolic link for libssl.so.3..."
-  if [ -f /usr/lib64/libssl.so.3 ]; then
-    sudo ln -sf /usr/lib64/libssl.so.3 /usr/lib/libssl.so.3
-  elif [ -f /usr/lib/x86_64-linux-gnu/libssl.so.3 ]; then
-    sudo ln -sf /usr/lib/x86_64-linux-gnu/libssl.so.3 /usr/lib/libssl.so.3
-  else
-    echo "libssl.so.3 not found! Installing manually..."
-    wget https://www.openssl.org/source/openssl-3.0.0.tar.gz
-    tar -xf openssl-3.0.0.tar.gz && cd openssl-3.0.0
-    ./config && make -j$(nproc) && sudo make install
-  fi
-  sudo ldconfig
-fi
-
 if [[ $(lsb_release -sc) == 'bookworm' ]]; then
   pip3 install --user --break-system-packages pytest-testinfra pytest
 else
@@ -84,10 +69,24 @@ if [ -z "${PXC57_PKG_VERSION}" ]; then
   echo "PXC57_PKG_VERSION environment variable needs to be set!"
   echo "export PXC57_PKG_VERSION=\"5.7.31-rel34-43.2\""
 fi
-tar xf "${TARBALL_NAME}"
-PXC_DIR_NAME=$(echo "${TARBALL_NAME}"|sed 's/.tar.gz$//'|sed 's/.deb$//'|sed 's/.rpm$//')
-export BASE_DIR="${PWD}/${PXC_DIR_NAME}"
-cp conf/*cnf $BASE_DIR/
+
+echo "non pro base dir setting.."
+if [[ "$PRO" != "true" ]]; then
+  echo "PRO IS NOT TRUE HERE!!!"
+  TARBALL_NAME=$(basename "$(find . -maxdepth 1 -name '*.tar.gz'|head -n1)")
+  if [ -z "${TARBALL_NAME}" ]; then
+    echo "Please put PXC tarball into this directory!"
+    exit 1
+  fi
+  tar xf "${TARBALL_NAME}"
+  PXC_DIR_NAME=$(echo "${TARBALL_NAME}"|sed 's/.tar.gz$//'|sed 's/.deb$//'|sed 's/.rpm$//')
+  export BASE_DIR="${PWD}/${PXC_DIR_NAME}"
+  echo "BASE_DIR is for non PRO $BASE_DIR"
+else
+  echo "PRO is TRUE HERE !!! base dir setting.."
+  export BASE_DIR="/usr/percona-server"
+  echo "BASE_DIR is for PRO $BASE_DIR"
+fi
 
 echo "Running tests..."
 echo "${PXC_DIR_NAME}"
