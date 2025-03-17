4ea626d0a622e4f5914425ca41ed0b82c9320f71
Grishma123-Eng <grishmashah121@gmail.com>
2025-03-17 13:31:37 +0530

    debugging for pxc pro tarballs

--- binary-tarball-tests/pxc/run.sh
+++ binary-tarball-tests/pxc/run.sh
@@ -23,11 +23,12 @@ if [ -f /etc/redhat-release ]; then
     sudo yum install -y percona-xtrabackup-24
   fi
 else
-  UCF_FORCE_CONFOLD=1 DEBIAN_FRONTEND=noninteractive sudo -E apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq -y install openssl
+  export DEBIAN_FRONTEND=noninteractive 
+  UCF_FORCE_CONFOLD=1 sudo -E apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq -y install openssl
   if [[ $(lsb_release -sc) == 'xenial' ]]; then
-    DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:deadsnakes/ppa
-    DEBIAN_FRONTEND=noninteractive sudo apt update
-    DEBIAN_FRONTEND=noninteractive sudo apt -y install python3.6
+    sudo add-apt-repository -y ppa:deadsnakes/ppa
+    sudo apt update
+    sudo apt -y install python3.6
     sudo rm /usr/bin/python3 && sudo ln -sf /usr/bin/python3.6 /usr/bin/python3
     wget -O get-pip.py "https://bootstrap.pypa.io/get-pip.py" && sudo python3 get-pip.py
     sudo apt install -y libcurl4-openssl-dev
