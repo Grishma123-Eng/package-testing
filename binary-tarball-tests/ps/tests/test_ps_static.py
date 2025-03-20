a695751ff1231d955ec3a6dce4a97539337eeb0d
Grishma123-Eng <grishmashah121@gmail.com>
2025-03-21 00:16:57 +0530

    debug

--- binary-tarball-tests/ps/tests/test_ps_static.py
+++ binary-tarball-tests/ps/tests/test_ps_static.py
@@ -57,11 +57,11 @@ def test_binaries_version(host,pro_fips_vars):
                 ps_revision_pro = str(ps_revision) + "-pro"
                 # Newer versions with Pro support
                 expected_mysql_output = (
-                    f"{base_dir}/bin/mysql  Ver {ps_version}-pro for Linux on x86_64 (Percona Server Pro (GPL), "
+                    f"{base_dir}/bin/mysql  Ver {ps_version_pro_percona}-pro for Linux on x86_64 (Percona Server Pro (GPL), "
                     f"Release {ps_version_percona}, Revision {ps_revision})"
                 )
                 expected_mysqld_output = (
-                    f"{base_dir}/bin/mysqld  Ver {ps_version}-pro for Linux on x86_64 (Percona Server Pro (GPL), "
+                    f"{base_dir}/bin/mysqld  Ver {ps_version_pro_percona}-pro for Linux on x86_64 (Percona Server Pro (GPL), "
                     f"Release {ps_version_percona}, Revision {ps_revision})"
                 )
         else: 
