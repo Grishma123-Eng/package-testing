3a285378cb09225c0b092afbf493e7e228396cce
Grishma123-Eng <grishmashah121@gmail.com>
2025-04-11 12:40:35 +0530

    revert back to main changes

--- binary-tarball-tests/pxc/test-pxc-binary-tarball-pro.groovy
+++ binary-tarball-tests/pxc/test-pxc-binary-tarball-pro.groovy
@@ -189,7 +189,7 @@ void run_test() {
     else
       sudo apt install -y git wget tar
     fi
-    git clone https://github.com/Grishma123-Eng/package-testing.git --branch pxc-final --depth 1
+    git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
     cd package-testing/binary-tarball-tests/pxc
     wget -q ${TARBALL_LINK}${TARBALL_NAME}
 
