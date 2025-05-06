865e4024e5384e870ddd58b74ad4c5aaaf5289c7
Grishma123-Eng <grishmashah121@gmail.com>
2025-05-06 14:10:53 +0530

    debug

--- molecule/test-pxc-binary-tarball-pro/tests/test_old_molecule.py
+++ molecule/test-pxc-binary-tarball-pro/tests/test_old_molecule.py
@@ -30,7 +30,7 @@ def test_load_env_vars_define_in_test(host):
             host.run(f"ls -ld {dir}")
 
 def test_regular_tarball(host, test_load_env_vars_define_in_test):
-    cmd = "cd /package-testing/binary-tarball-tests/pxc/Percona-XtraDB-Cluster-Pro && ./run.sh"
+    cmd = "cd package-testing/binary-tarball-tests/pxc/ && ./run.sh"
     result = host.run(cmd)
     print(result.stdout)
     print(result.stderr)
@@ -40,7 +40,7 @@ def test_minimal_tarball(host, test_load_env_vars_define_in_test):
     with host.sudo():
         cmd = f"sed -i 's|^\(BASE_DIR=.*\)/$|\1/-minimal|' /etc/environment"
         result = host.run(cmd)
-    cmd = "cd /package-testing/binary-tarball-tests/pxc/Percona-XtraDB-Cluster-Pro/ && ./run.sh"
+    cmd = "cd package-testing/binary-tarball-tests/pxc/ && ./run.sh"
     result = host.run(cmd)
     print(result.stdout)
     print(result.stderr)
