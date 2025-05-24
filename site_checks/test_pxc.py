f7282fd3999cd287f51a8d6ef7ab7fd8c86fc61e
Grishma123-Eng <grishmashah121@gmail.com>
2025-05-24 16:33:56 +0530

    debug

--- site_checks/test_pxc.py
+++ site_checks/test_pxc.py
@@ -29,7 +29,7 @@ DATA_VERSION=''.join(PXC_VER_FULL.split('.')[:2])
 
 BASE_PATH = f"https://downloads.percona.com/downloads/Percona-Xtradb-Cluster-{DATA_VERSION}/Percona-Xtradb-Cluster-{PXC_VER_UPSTREAM}"
 
-# Create list of supported software files and PXC 57 specific version numbers
+# Create package_list of supported software files and PXC 57 specific version numbers
 if version.parse(PXC_VER_UPSTREAM) >= version.parse("8.1.0"):
     DEB_SOFTWARE_FILES=['bullseye', 'bookworm', 'focal', 'jammy', 'noble']
     RHEL_SOFTWARE_FILES=[ 'redhat/8', 'redhat/9']
@@ -50,55 +50,20 @@ SOFTWARE_FILES=DEB_SOFTWARE_FILES+RHEL_SOFTWARE_FILES+['binary','source']
 RHEL_EL={'redhat/8':'8', 'redhat/9':'9'}
 
 def get_package_tuples():
-    list = []
+    package_list = []
     for software_file in SOFTWARE_FILES:
       #  data = 'version_files=Percona-XtraDB-Cluster-' + PXC_VER_UPSTREAM + '&software_files=' + software_file
        # req = requests.post("https://www.percona.com/products-api.php",data=data,headers = {"content-type": "application/x-www-form-urlencoded; charset=UTF-8"})
       #  freq.status_code == 200
        # freq.text != '[]', software_file
         # Check binary tarballs
-        if "binary" in SOFTWARE_FILES:
-            glibc_versions = ["2.35"] if version.parse(PXC_VER_UPSTREAM) < version.parse("8.0.0") else ["2.28", "2.31", "2.34", "2.35"]
-            for glibc_version in glibc_versions:
-                if version.parse(PXC_VER_UPSTREAM) > version.parse("8.0.0"):
-                    for suffix in ["", "-minimal"]:
-                        filename = [
-                            f"Percona-XtraDB-Cluster_{PXC_VER_FULL}_Linux.x86_64.glibc{glibc_version}{suffix}.tar.gz",
-                            f"Percona-XtraDB-Cluster_{PXC_VER_FULL}_Linux.x86_64.glibc{glibc_version}.tar.gz"]
-                        for file in filename:
-                            list.append(("binary", filename, f"{BASE_PATH}/binary/tarball/{file}"))
-
-                elif version.parse(PXC_VER_UPSTREAM) > version.parse("5.7.0") and version.parse(PXC_VER_UPSTREAM) < version.parse("8.0.0"):
-                    for filename in [
-                        f"Percona-XtraDB-Cluster_" + PXC_VER_UPSTREAM + "-rel" + PXC57_INNODB + "-" + PXC57_WSREP_PROV_VER + "." + PXC_BUILD_NUM + ".Linux.x86_64.glibc"+glibc_version+"-minimal.tar.gz",
-                        f"Percona-XtraDB-Cluster_" + PXC_VER_UPSTREAM + "-rel" + PXC57_INNODB + "-" + PXC57_WSREP_PROV_VER + "." + PXC_BUILD_NUM + ".Linux.x86_64.glibc"+glibc_version+".tar.gz"
-                    ]:
-                        list.append(("binary", filename, f"{BASE_PATH}/binary/tarball/{file}"))
-
-        # Check source tarballs
-        elif software_file == 'source':
-            if version.parse(PXC_VER_UPSTREAM) > version.parse("8.0.0"):
-                for filename in [
-                    f"Percona-XtraDB-Cluster-" + PXC_VER_PERCONA + ".tar.gz" ,
-                    f"percona-xtradb-cluster_" + PXC_VER_PERCONA + ".orig.tar.gz" ,
-                    f"percona-xtradb-cluster-" + PXC_VER_FULL  + ".generic.src.rpm"
-                ]:
-                    list.append(("source", filename, f"{BASE_PATH}/source/tarball/{filename}"))
-
-            elif version.parse(PXC_VER_UPSTREAM) > version.parse("5.7.0") and version.parse(PXC_VER_UPSTREAM) < version.parse("8.0.0"):
-                for filename in [
-                    f"Percona-XtraDB-Cluster-" + PXC_VER_PERCONA + ".tar.gz",
-                    f"percona-xtradb-cluster-5.7_" + PXC_VER_PERCONA + ".orig.tar.gz",
-                    f"Percona-XtraDB-Cluster-57-" + PXC_VER_FULL + ".generic.src.rpm"
-                ]:
-                    list.append(("source", filename, f"{BASE_PATH}/source/tarball/{filename}"))
-
+       
         # Test packages for every OS
-        else:
+            #if version.parse(PXC_VER_UPSTREAM) > version.parse("8.0.0"):
+        if software_file in DEB_SOFTWARE_FILES:
+            pxc_deb_name_suffix=PXC_VER_PERCONA + "-" + PXC_BUILD_NUM + "." + software_file + "_amd64.deb"
             if version.parse(PXC_VER_UPSTREAM) > version.parse("8.0.0"):
-                if software_file in DEB_SOFTWARE_FILES:
-                    pxc_deb_name_suffix=PXC_VER_PERCONA + "-" + PXC_BUILD_NUM + "." + software_file + "_amd64.deb"
-                    deb_files = [
+                deb_files = [
                         f"percona-xtradb-cluster-server_" + pxc_deb_name_suffix ,
                         f"percona-xtradb-cluster-test_" + pxc_deb_name_suffix ,
                         f"percona-xtradb-cluster-client_" + pxc_deb_name_suffix ,
@@ -110,14 +75,30 @@ def get_package_tuples():
                         f"percona-xtradb-cluster-source_" + pxc_deb_name_suffix ,
                         f"percona-xtradb-cluster-common_" + pxc_deb_name_suffix ,
                         f"percona-xtradb-cluster-dbg_" + pxc_deb_name_suffix ,
-                        ]
-                    for file in deb_files:
-                        list.append((software_file, file, f"{BASE_PATH}/binary/debian/{software_file}/x86_64/{file}"))
+                ]
+            else:
+                deb_files = [
+                    f"libperconaserverclient20-dev_{pxc_deb_name_suffix}",
+                    f"libperconaserverclient20_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-source-5.7_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-common-5.7_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-57_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-server-5.7_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-test-5.7_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-client-5.7_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-garbd-5.7_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-full-57_{pxc_deb_name_suffix}",
+                    f"percona-xtradb-cluster-5.7-dbg_{pxc_deb_name_suffix}"
+                ]
+            for file in deb_files:
+                package_list.append((software_file, file, f"{BASE_PATH}/binary/debian/{software_file}/x86_64/{file}"))
 
-                if software_file in RHEL_SOFTWARE_FILES:
-                    pxc_rpm_name_suffix=PXC_VER_PERCONA + "." + PXC_BUILD_NUM + "." + RHEL_EL[software_file] + ".x86_64.rpm"
-                    el = RHEL_EL[software_file]
-                    rpm_files = [
+        elif software_file in RHEL_SOFTWARE_FILES:
+           # pxc_rpm_name_suffix=PXC_VER_PERCONA + "." + PXC_BUILD_NUM + "." + RHEL_EL[software_file] + ".x86_64.rpm"
+            el = RHEL_EL[software_file]
+            pxc_rpm_name_suffix = f"{PXC_VER_PERCONA}.{PXC_BUILD_NUM}.{el}.x86_64.rpm"
+            if version.parse(PXC_VER_UPSTREAM) > version.parse("8.0.0"):
+                rpm_files = [
                         f"percona-xtradb-cluster-server-" + pxc_rpm_name_suffix ,
                         f"percona-xtradb-cluster-test-" + pxc_rpm_name_suffix ,
                         f"percona-xtradb-cluster-client-" + pxc_rpm_name_suffix ,
@@ -127,54 +108,27 @@ def get_package_tuples():
                         f"percona-xtradb-cluster-devel-" + pxc_rpm_name_suffix ,
                         f"percona-xtradb-cluster-shared-" + pxc_rpm_name_suffix ,
                         f"percona-xtradb-cluster-icu-data-files-" + pxc_rpm_name_suffix 
-                    ]
-                    if software_file != "redhat/9":
+                ]
+                if software_file != "redhat/9":
                         rpm_files.append(f"percona-server-shared-compat-{pxc_rpm_name_suffix}")
-                    for file in rpm_files:
-                        list.append((software_file, file, f"{BASE_PATH}/binary/redhat/{el}/x86_64/{file}"))
-
+            else:
+                rpm_files = [
+                    f"Percona-XtraDB-Cluster-server-57-{pxc_rpm_name_suffix}",
+                    f"Percona-XtraDB-Cluster-test-57-{pxc_rpm_name_suffix}",
+                    f"Percona-XtraDB-Cluster-client-57-{pxc_rpm_name_suffix}",
+                    f"Percona-XtraDB-Cluster-garbd-57-{pxc_rpm_name_suffix}",
+                    f"Percona-XtraDB-Cluster-full-57-{pxc_rpm_name_suffix}",
+                    f"Percona-XtraDB-Cluster-devel-57-{pxc_rpm_name_suffix}",
+                    f"Percona-XtraDB-Cluster-shared-57-{pxc_rpm_name_suffix}",
+                    f"Percona-XtraDB-Cluster-57-debuginfo-{pxc_rpm_name_suffix}"
+                ]
+                if software_file != "redhat/9":
+                    rpm_files.append(f"percona-server-shared-compat-{pxc_rpm_name_suffix}")
+            for file in rpm_files:
+                package_list.append((software_file, file, f"{BASE_PATH}/binary/redhat/{el}/x86_64/{file}"))
     
-            elif version.parse(PXC_VER_UPSTREAM) > version.parse("5.7.0") and version.parse(PXC_VER_UPSTREAM) < version.parse("8.0.0"):
-                assert any("dbg" in f or "debug" in f for f in deb_files + rpm_files), \
-                    "Neither 'dbg' nor 'debug' found in expected filenames"
-                if software_file in DEB_SOFTWARE_FILES:
-                    pxc_deb_name_suffix=PXC_VER_PERCONA + "-" + PXC_BUILD_NUM + "." + software_file + "_amd64.deb"
-                    deb_files = [
-                    f"libperconaserverclient20-dev_" + pxc_deb_name_suffix ,
-                    f"libperconaserverclient20_" + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-source-5.7_" + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-common-5.7_" + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-57_" + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-server-5.7_" + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-test-5.7_"  + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-client-5.7_" + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-garbd-5.7_" + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-full-57_" + pxc_deb_name_suffix ,
-                    f"percona-xtradb-cluster-5.7-dbg_" + pxc_deb_name_suffix ]
-
-                    for file in deb_files:
-                        list.append((software_file, file, f"{BASE_PATH}/binary/debian/{software_file}/x86_64/{file}"))
-
-                if software_file in RHEL_SOFTWARE_FILES:
-                    pxc_rpm_name_suffix=PXC_VER_PERCONA + "." + PXC_BUILD_NUM + "." + RHEL_EL[software_file] + ".x86_64.rpm"
-                    el = RHEL_EL[software_file]
-                    rpm_files = [
-                    f"Percona-XtraDB-Cluster-server-57-" + pxc_rpm_name_suffix ,
-                    f"Percona-XtraDB-Cluster-test-57-" + pxc_rpm_name_suffix ,
-                    f"Percona-XtraDB-Cluster-client-57-" + pxc_rpm_name_suffix ,
-                    f"Percona-XtraDB-Cluster-garbd-57-" + pxc_rpm_name_suffix ,
-                    f"Percona-XtraDB-Cluster-full-57-" + pxc_rpm_name_suffix ,
-                    f"Percona-XtraDB-Cluster-devel-57-" + pxc_rpm_name_suffix ,
-                    f"Percona-XtraDB-Cluster-shared-57-" + pxc_rpm_name_suffix ,
-                    f"Percona-XtraDB-Cluster-57-debuginfo-" + pxc_rpm_name_suffix ]
-                    if software_file != "redhat/9":
-                        rpm_files.append(f"percona-server-shared-compat-{pxc_rpm_name_suffix}")
-                    for file in rpm_files:
-                        list.append((software_file, file, f"{BASE_PATH}/binary/redhat/{el}/x86_64/{file}"))
-                
-    return list
-
-
+    return package_list
+            
 LIST_OF_PACKAGES = get_package_tuples()
 
 # Check that every link from website is working (200 reply and has some content-length)
